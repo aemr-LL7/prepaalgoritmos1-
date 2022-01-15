3a695eed7abb008998fa5e9c70bbcba453fd546c
aemr-LL7 <a.marquez2020@correo.unimet.edu.ve>
2022-01-15 13:43:58 -0400

    Calculadora añadida

--- holaMundo.py
+++ holaMundo.py
@@ -1,1 +1,6 @@
 print("Hola Mundo!\n -Welcome brah")
+
+number_one = int(input("Please insert a number to add: "))
+number_two = float(input("Insert a second number: "))
+
+result = print(number_one + number_two)
